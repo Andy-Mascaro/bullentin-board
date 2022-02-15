d4dc76e50f03436a163bb55330dbd45dd7b7bc5b
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-02-14 17:41:30 -0800

    added fetch utils and supabase const.

---
+++ fetch-utils.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://nschbtdnjkjkoshunkhk.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zY2hidGRuamtqa29zaHVua2hrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQ0MzYwMjAsImV4cCI6MTk2MDAxMjAyMH0.mwm25ysgqkbA7ZMQcYTguKcFC-qRnmiDHgK2PxCROPk';
+
+const client = supabase.creatClient(SUPABASE_URL, SUPABASE_KEY);
+
