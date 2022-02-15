0e787b6a24d728be376d9b506d9419e28897e35e
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-02-15 10:45:35 -0800

    added to render-utils

--- render-utils.js
+++ render-utils.js
@@ -2,5 +2,18 @@ export function renderSheets(post) {
     const div = document.createElement('div');
     div.classList.add('sheets');
 
-    const
+    const h2 = document.createElement('h2');
+    h2.textContent = post.title;
+
+    const p1 = document.createElement('p');
+    p1.textContent = post.description;
+    const p2 = document.createElement('p');
+    p2.textContent = post.contact;
+    p2.classList.add('contact');
+
+    div.append(h2, p1, p2);
+
+    return div;
+
+
 }
